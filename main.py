fab18dfe2b0e379eac0251ba91236a85f0dc513a
Andersondafonseca <127258727+Andersondafonseca@users.noreply.github.com>
2025-04-23 22:25:31 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -19,7 +19,3 @@ async def make_call(request: CallRequest):
     )
     return {"status": "ligando", "sid": call.sid}
 
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8080))
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
